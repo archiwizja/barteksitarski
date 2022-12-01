4ba0adc9c963b804981c6534e8e5c54fffc31eae
Przemek <fikasp@gmail.com>
2022-12-01 22:48:20 +0100

    add logo to routing

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,57 +1,51 @@
-const navLinks = document.querySelectorAll('.nav__link')
-const mainSection = document.querySelector(".main")
-
 let home = ''
 let offer = ''
 let contact = ''
 let notFound = `<h1>404</h1>`
+let routes = {};
+
+const navLinks = document.querySelectorAll('.nav__link')
+const mainSection = document.querySelector(".main")
+
+
+function render(path) {
+    console.log(`Render: ${path}`);
+    mainSection.innerHTML = routes[path] || notFound
+}
 
 async function loadPage(page) {
-    console.log(`Loading: templates/${page}.html start`);
-
     const response = await fetch(`templates/${page}.html`)
-    const resHtml = await response.text()
-
-    console.log(`Loading: templates/${page}.html finished`);
-    return resHtml
+    const responseHtml = await response.text()
+    return responseHtml
 }
 
 async function loadAllPages() {
     home = await loadPage('home');
     offer = await loadPage('offer');
     contact = await loadPage('contact');
-    console.log("Pages loaded:");
 };
 
 async function main() {
     await loadAllPages()
-    console.log(home);
-    console.log(offer);
-    console.log(contact);
-
-    console.log("Main");
-    mainSection.innerHTML = home
-
-    const routes = {
+    routes = {
         "/": home,
         "/offer": offer,
         "/contact": contact,
     };
 
+    render("/")
+
     window.addEventListener("popstate", e =>
-    render(new URL(window.location.href).pathname)
+        render(new URL(window.location.href).pathname)
     )
 
     navLinks.forEach(link => {
-        console.log(link)
+
         link.addEventListener("click", evt => {
             evt.preventDefault()
-
-            const {pathname: path} = new URL(evt.target.href);
-            console.log(path);
-
+            const {pathname: path} = new URL(evt.target.closest('a').href);
             window.history.pushState({}, path, path)
-            mainSection.innerHTML = routes[path]
+            render(path)
         })
     })
 }
